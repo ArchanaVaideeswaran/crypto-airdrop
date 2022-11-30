de46a72c9ca89ee22ce2e1f6280ec65baf3c4017
Archana Vaideeswaran <46862596+ArchanaVaideeswaran@users.noreply.github.com>
2022-11-30 18:53:23 +0530

    contract and deploy script complete

---
+++ contracts/MerkleAirdrop.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleAirdrop is ERC20 {
+
+    bytes32 public immutable merkleRoot;
+
+    mapping(address => bool) public hasClaimed;
+
+    event Claimed(address indexed to, uint amount);
+
+    constructor(bytes32 _merkleRoot) ERC20("Merkle Airdrop Token", "MAT") {
+        merkleRoot = _merkleRoot;
+    }
+
+    function claim(address to, uint amount, bytes32[] calldata proof) public {
+        require(!hasClaimed[to], "Already claimed");
+
+        // Verify merkle proof, or revert if not in tree
+        bytes32 leaf = keccak256(abi.encodePacked(to, amount));
+        bool isValidLeaf = MerkleProof.verify(proof, merkleRoot, leaf);
+        require(isValidLeaf, "Not in merkle tree");
+
+        // Set address to claimed
+        hasClaimed[to] = true;
+
+        // Mint tokens to address
+        _mint(to, amount);
+
+        // Emit claim event
+        emit Claimed(to, amount);
+    }
+}
--- scripts/deploy-airdrop.ts
+++ scripts/deploy-airdrop.ts
@@ -1,4 +1,35 @@
+import { ethers } from "hardhat";
+import { Recepient, generateTree } from "./merkle-tree-generator";
+import * as fs from "fs";
 
-async function main() {}
+
+async function main() {
+    console.log("\n----------Deploying Merkle Airdrop Token----------\n");
+
+    const [ owner, ...accounts ] = await ethers.getSigners();
+    console.log("Deployer: ", owner);
+
+    let recepients: Recepient[] = [];
+    const decimals = 18;
+    const length = accounts.length > 5 ? 5 : accounts.length;
+    for(let i = 0; i < length; i++) {
+        const user = accounts[i];
+        recepients.push({
+            address: user.address, 
+            value: ethers.utils.parseUnits("10", decimals).toString()
+        });
+    }
+
+    const merkleRoot = generateTree(recepients);
+
+    console.log("main(): Merkle root: ", merkleRoot);
+
+    const Airdrop = await ethers.getContractFactory("MerkleAirdrop");
+    const airdrop = await Airdrop.deploy(merkleRoot);
+    await airdrop.deployed();
+
+    console.log("\n----------Deployed Merkle Airdrop Token----------\n");
+    console.log("Merkle Airdrop Token deployed at: ", airdrop.address);
+}
 
 main();
